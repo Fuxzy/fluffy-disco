d84b5a858b45d8ec3f8282077e8538fb2b3c42cc
Fuxzy <34891539+Fuxzy@users.noreply.github.com>
2017-12-29 21:22:59 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const PREFIX = "!";
+const YTDL = require("ytdl-core");
+
+var bot = new Discord.Client();
+const size    = 64
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+var servers = {};
+
+function play(connection, message) {
+  var server = servers[message.guild.id];
+
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+  server.queue.shift();
+   
+  server.dispatcher.on("end", function () {
+    if (server.queue[0]) play (connection, message);
+    else connection.disconnect();
+  });
+}
+
+function changeColor() {
+  for (let index = 0; index < 1; ++index) {		
+    bot.guilds.get("395216948469694466").roles.find('name', "Vice Chairman").setColor(rainbow[place])
+    bot.guilds.get("395216948469694466").roles.find('name', "Chairman").setColor(rainbow[place])
+		.catch(console.error);
+		
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.username}!`);
+  if(60000 < 60000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
+  setInterval(changeColor, 60000);
+});
+
+bot.login(process.env.BOT_TOKEN);
